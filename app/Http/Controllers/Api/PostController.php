8c302bb88ff8c9c32ec1ed63d7e59476e3fd8169
AlessP93 <alespiscino@libero.it>
2022-07-26 16:08:37 +0200

    fix api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::where('published', true)->with(['category', 'tags', 'user'])->get();
+          $posts = Post::where('published', true)->with(['category', 'tags', 'user'])->get();
 
         return response()->json($posts);
     }
